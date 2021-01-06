24a4ef32613f507dff868e607034977adaa45653
dasco69 <claude.proaudiovisuel@gmail.com>
2021-01-06 14:46:40 +0100

    first commit

--- api/keys.py
+++ api/keys.py
@@ -1,6 +1,6 @@
 keys = dict(
-    consumer_key =          'i44rGduIIdQ4xqvPZSQvx9OxU',
-    consumer_secret =       'Xu9TepQ45ATyVviHxWNzQqJZs2JPApfM7dI90TPCaMf9knZ08H',
-    access_token =          '1258303471759110146-OkayfBW10lTHr1XpeZVjVYJzfqX4bc',
-    access_token_secret =   'NNGsYFrPPiUqnLuBNgpHwy1MRRn9oyD0nP5qPFNUAwE59',
+    consumer_key =          'key',
+    consumer_secret =       'secret',
+    access_token =          'access token',
+    access_token_secret =   'secret token',
 )
